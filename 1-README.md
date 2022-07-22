2189eb7c02b326e40586fc51af35855cae3471ec
Balvis <107074621+bralvis2@users.noreply.github.com>
2022-07-21 22:06:14 -0500

    Generates READ ME FILE

---
+++ 1-README.md
@@ -0,0 +1,31 @@
+# ReadMe-Generator
+
+### Licenses
+
+## Table of Contents
+* [Description](#description)
+
+* [Installation](#installation-instructions)
+
+* [Usage-Information](#usage-information)
+
+* [Contribution](#contribution-guidelines)
+
+* [Test-Instructions](#test-instructions)
+
+* [Questions](#questions)
+
+## Description
+
+## Installation Instructions
+
+## Usage Information
+
+## Contribution Guidelines
+
+## Test Instructions
+
+## Questions 
+
+* 
+* 
--- index.js
+++ index.js
@@ -2,64 +2,118 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [
+const generateReadMe = ({ title, description, installation, usage, contribution, test, license, github, email}) =>
+`# ${title}
+
+### Licenses
+${license}
+
+## Table of Contents
+* [Description](#description)
+
+* [Installation](#installation-instructions)
+
+* [Usage-Information](#usage-information)
+
+* [Contribution](#contribution-guidelines)
+
+* [Test-Instructions](#test-instructions)
+
+* [Questions](#questions)
+
+## Description
+${description}
+
+## Installation Instructions
+${installation}
+
+## Usage Information
+${usage}
+
+## Contribution Guidelines
+${contribution}
+
+## Test Instructions
+${test}
+
+## Questions 
+
+* ${github}
+* ${email}`;
+
+inquirer
+    .prompt ([
     {
         type: 'input',
-		name: 'title',
-		message: 'What is the title of your project?',
+        name: 'title',
+        message: 'What is the title of your project?',
     },
     {
         type: 'input',
-		name: 'description',
-		message: 'Describe your project.',
+        name: 'description',
+        message: 'Describe your project.',
     },
     {
         type: 'input',
-		name: 'instillation',
-		message: 'Describe the instillation process.',
+        name: 'installation',
+        message: 'Describe the instillation process.',
     },
     {
         type: 'input',
-		name: 'usage',
-		message: 'Describe instructions for usage.',
+        name: 'usage',
+        message: 'Describe instructions for usage.',
     },
     {
         type: 'input',
-		name: 'contribution',
-		message: 'Describe how others can contribute to your project.',
+        name: 'contribution',
+        message: 'Describe how others can contribute to your project.',
     },
     {
         type: 'input',
-		name: 'test',
-		message: 'Describe the test instructions',
+        name: 'test',
+        message: 'Describe the test instructions',
     },
     {
         type: 'checkbox',
-		name: 'license',
-		message: 'Select the licences you would like to include.',
-        choices: [MIT, ]
+        name: 'license',
+        message: 'Select the licences you would like to include.',
+        choices: [
+            {
+                name: 'MIT',
+                value: {
+                    name: 'MIT',
+                    link: 'MIT',
+                    url: 'https://opensource.org/licenses/MIT',
+                    color: 'yellow',
+                },
+            },
+            {
+                name: 'Apache 2.0 License',
+                value: {
+                    name: 'Apache 2.0 License',
+                    link: 'Apache%202.0',
+                    url: 'https://opensource.org/licenses/Apache-2.0',
+                    color: 'lime-green',
+                },
+            }
+        ]
     },
     {
         type: 'input',
-		name: 'github',
-		message: 'Enter your github user name',
+        name: 'github',
+        message: 'Enter your github user name',
     },
     {
         type: 'input',
-		name: 'email',
-		message: 'Enter your email.',
+        name: 'email',
+        message: 'Enter your email.',
     },
+])
+.then((answers) => {
+    const readMeContent = generateReadMe (answers);
 
-];
+    fs.writeFile('generatedREADME.md', readMeContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+});
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-
-}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
