48f6c40903754c2113ab8fba37b7b28e2e3c70b3
Balvis <107074621+bralvis2@users.noreply.github.com>
2022-07-25 18:05:38 -0500

    License Badge & GitHub Avatar

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateReadMe = ({ title, description, installation, usage, contribution, test, license, github, email}) =>
-`# ${title}
+// Generates the Content of the README.md 
+const generateReadMe = ({ title, description, installation, usage, contribution, test, license, github, email }) =>
+    `# ${title}
 
 ### Licenses
-${license}
+${renderBadge(license)}
 
 ## Table of Contents
 * [Description](#description)
@@ -38,82 +39,82 @@ ${test}
 
 ## Questions 
 
-* ${github}
+[![](https://github.com/${github}.png?size=50)](https://github.com/${github})
+
+[Github](https://github.com/${github})
 * ${email}`;
 
+// Array of Prompts
 inquirer
-    .prompt ([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Describe your project.',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Describe the instillation process.',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Describe instructions for usage.',
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Describe how others can contribute to your project.',
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Describe the test instructions',
-    },
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Select the licences you would like to include.',
-        choices: [
-            {
-                name: 'MIT',
-                value: {
-                    name: 'MIT',
-                    link: 'MIT',
-                    url: 'https://opensource.org/licenses/MIT',
-                    color: 'yellow',
-                },
-            },
-            {
-                name: 'Apache 2.0 License',
-                value: {
-                    name: 'Apache 2.0 License',
-                    link: 'Apache%202.0',
-                    url: 'https://opensource.org/licenses/Apache-2.0',
-                    color: 'lime-green',
-                },
-            }
-        ]
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your github user name',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email.',
-    },
-])
-.then((answers) => {
-    const readMeContent = generateReadMe (answers);
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe your project.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Describe the instillation process.',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Describe instructions for usage.',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Describe how others can contribute to your project.',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Describe the test instructions',
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Select the licences you would like to include.',
+            choices: ['Mit','Apache', 'Mozilla']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your github user name',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email.',
+        },
+    ])
 
-    fs.writeFile('generatedREADME.md', readMeContent, (err) =>
-        err ? console.log(err) : console.log('Successfully created README.md!')
-    );
-});
+    // Generates a README with the answers from the prompts
+    .then((answers) => {
+        const readMeContent = generateReadMe(answers);
 
+        fs.writeFile('generatedREADME.md', readMeContent, (err) =>
+            // if the README.md was generated, users get a success message. If not, an error msg.
+            err ? console.log(err) : console.log('Successfully created README.md!')
+        );
+    });
+
+// Licenses 
+function renderBadge(license) {
+
+    if (license == 'Mit') {
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)'
+    } else if (license == 'Apache') {
+        return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license == 'Mozilla') {
+        return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+    } else {
+        return ''
+    }
+}
